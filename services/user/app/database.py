5b7dc1ef19c4086e1c274b62c5aebf77123fe172
pranav <psoniradhe177@gmail.com>
2026-06-18 16:47:25 +0530

    removed irrelevant env from notification-svc

--- services/user/app/database.py
+++ services/user/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/userdb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://super:password@localhost:5432/userdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
